60087eece0018af06982f90ff24ae34c2968da3c
Bizhan <bizhan.gholikhamseh@gmail.com>
2021-02-14 20:15:34 +0000

    loop.py Looping

---
+++ loop.py
@@ -0,0 +1,20 @@
+# for item in iterable_object:
+#   do something
+#example 2
+for x in range(1,10):
+  print(x)
+
+#example 1
+for x in range(10):  
+  print(x)
+
+#skip numbers by 2
+for x in range(1,10,2):
+  print(x)
+
+#reverse range
+for x in range(7,0,-1):
+  print(x)
+
+num = range(1,5)
+print(num) # the output is range(1,5)
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import  random
+rand_num = random.randint(0,2)
+if rand_num == 0:
+  computer = "rock"
+elif rand_num == 1:
+  computer = "paper"
+else:
+  computer = 'sissors'
+
+print(computer)
