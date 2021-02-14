2d7101b850a4ec3f6b505d3880751812ee74311f
Bizhan <bizhan.gholikhamseh@gmail.com>
2021-02-14 20:56:15 +0000

    [f string]

--- loop2.py
+++ loop2.py
@@ -4,3 +4,12 @@ times = int(times)
 # f string: how {time} is replaces with actual number
 for time in range(times):
   print(f" time {time +1}: number")
+
+for num in range(1,21):
+  if num == 4 or num == 13:
+    state = "unlucky"
+  elif num % 2 == 0:
+    state = "even"
+  else:
+    state = "odd"
+  print(f"{num} is {state}")
