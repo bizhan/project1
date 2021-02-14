3e5c24872da081530f60a73053c523ae7d457a29
Bizhan <bizhan.gholikhamseh@gmail.com>
2021-02-14 22:58:41 +0000

    [pep8]: adding while.py and how to run autopep8 to format the source code.

---
+++ while.py
@@ -0,0 +1,12 @@
+# while im_tired:
+#   seek caffeine
+
+# for num in range(1,11):
+#   print("\U0001f600" * num)
+
+msg = "hello"
+while msg != "STOP":
+  msg = input(f"{msg}\n")
+
+# Generates pep8 style source code.
+# autopep8 --in-place ugly-code.py
