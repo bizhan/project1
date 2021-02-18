62e278656800219c244670426b60ed78a7105626
Bizhan <bizhan.gholikhamseh@gmail.com>
2021-02-18 16:08:08 +0000

    [reversed]

---
+++ function/reversed.py
@@ -0,0 +1,9 @@
+#reversed
+print('hello')
+# returns a reversed iterator
+nums = [1,2,3,4]
+revs =nums.reverse() # ?????
+print(revs)
+print(''.join(list(reversed('hello'))))
+
+#Not every iteratable supports slices so reversed might be used.
