3310c89f5318ba56a2626ec77180b66265d3d445
Bizhan <bizhan.gholikhamseh@gmail.com>
2021-02-19 22:46:14 +0000

    many oops related items

--- oop/super_method.py
+++ oop/super_method.py
@@ -1,60 +1,26 @@
-#inheritance
-#passing parent class as an argument to the definition of clild
-
 class Animal:
+  def __init__(self, name, species):
+    self.name = name
+    self.species = species
   def make_sound(self, sound):
-    print(sound)
-
-  cool = True
+    print(f'this is animal says {sound}')
+  def __repr__(self):
+    return f'{self.name} {self.species}'
 
 class Cat(Animal):
-  pass
+  def __init__(self, name, species, breed, toy):
+    super().__init__(name, species)
+    self.breed = breed
+    self.toy = toy
 
-gandalf = Cat()
-gandalf.make_sound('meow')
-gandalf.cool
-
-#isinstance
-print(isinstance(gandalf, Animal))
-
-#Properties
-
-class Human:
-  def __init__(self,first,last,age):
-    self.first = first
-    self.last = last
-    if (age >= 0):
-      self._age = age
-    else:
-      self._age = age
-  @property
-  def age(self):
-    return self._age
-  @age.setter
-  def age(self, value):
-    self._age = value
-
-  @property
-  def full_name(self):
-    return f'{self.first} {self.last}'
-
-  full_name.setter
-  def  full_name(self, name):
-    self.first, self.last = name.split(" ")
-
-jane = Human('Jane', 'Goodall', -9)
-print(jane.age)
-jane.age = 20
-print(jane.age)
-jane.full_name ="Bizhan Gholikhamseh"
-print(jane.full_name)
-print(jane.__dict__)
+blue = Cat('blue', 'Cat', 'Scottish Fold', 'String')
+print(blue)
 
 class User:
   active_users = 0
   @classmethod
   def display_active_users(cls):
-    print(cls)
+    return f'There are currently {cls.active_users} active users'
   @classmethod
   def from_string(cls, data_str):
     first,last,age = data_str.split(',')
@@ -73,7 +39,8 @@ class User:
   def full_name(self):
     User.active_users -= 1
     return f'{self.first} {self.last}'
-
+    
+ 
   def initials(self):
     return f'{self.first[0]} {self.last[0]}'
   def is_senior(self):
@@ -89,9 +56,16 @@ class Moderator(User):
   def remove_post(self):
     return f'{self.full_name()} removed a post from {self.community} community'
 
+print(User.display_active_users())
+u1 = User('Tom', 'Garcia', 35)
+print(User.display_active_users())
 jasmine = Moderator('Jasmine', 'O Conner', 30, 'Piano')
-
+print(User.display_active_users())
+print(jasmine.full_name())
 print(jasmine.community)
   
+
+
+
 
 
